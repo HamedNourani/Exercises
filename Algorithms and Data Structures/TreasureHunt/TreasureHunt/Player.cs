9e0887a77da6f1929bd63eea4ee41530f7972dc3
Hamed Nourani <hamednourani1026@gmail.com>
2023-10-11 18:16:34 +0330

    Add player class

---
+++ Algorithms and Data Structures/TreasureHunt/TreasureHunt/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TreasureHunt
+{
+    public class Player
+    {
+        private int _x;
+        private int _y;
+        private int _moves;
+
+        public int X
+        {
+            get => _x;
+            set => _x = value;
+        }
+
+        public int Y
+        {
+            get => _y;
+            set => _y = value;
+        }
+
+        public int Moves
+        {
+            get => _moves;
+            set => _moves = value;
+        }
+
+        public Player(int x, int y, int moves)
+        {
+            _x = x;
+            _y = y;
+            _moves = moves;
+        }
+    }
+}
